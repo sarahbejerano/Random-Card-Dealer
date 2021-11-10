74c44e3c02bf0510dfc2b0d49a5b3a9425eee1f5
sarahbejerano <bejeranosarah@gmail.com>
2021-11-10 21:44:53 +0000

    working on generate function

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
-/* eslint-disable */
-
 import "./style.css";
 
-window.onload = function() {
-  //write your code here
-  console.log("Everything is Woking");
-};
+const symbols = ["\u25ca", "\u2660", "\u2663", "\u2665", "\u2666"];
+const numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+let topSymbol = document.querySelector("#topSymbol");
+let centerNumber = document.querySelector("#centerNumber");
+let bottomSymbol = document.querySelector("#bottomSymbol");
+
+function name(params) {}
+
+window.onload = function() {};
